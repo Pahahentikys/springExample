96e982f9914b9abf5c7e3d31175e9086a8a96ffe
Pahahentikys <Pavel1995.rus@yandex.ru>
2018-02-07 02:07:46 +0300

    Закинул тестовые email-сервисы в IOC-контейнер через @Bean в классе: ServiceConfig.

--- src/main/java/config/ServiceConfig.java
+++ src/main/java/config/ServiceConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import serv.EmailMsgService;
 import servImpl.EmailMsgServiceImpl;
+import servImpl.GmailEmailMsgServiceImpl;
+import servImpl.YahooEmailMsgServiceImpl;
+import servImpl.YandexEmailMsgServiceImpl;
 
 @Configuration
 public class ServiceConfig {
@@ -11,5 +14,20 @@ public class ServiceConfig {
     @Bean
     EmailMsgService emailMsgService(){
         return new EmailMsgServiceImpl();
+    }
+
+    @Bean(name = "YahooEmailMsgService")
+    EmailMsgService yahooEmailService(){
+        return new YahooEmailMsgServiceImpl();
+    }
+
+    @Bean(name = "YandexEmailMsgService")
+    EmailMsgService yandexEmailService(){
+        return new YandexEmailMsgServiceImpl();
+    }
+
+    @Bean(name = "GmailEmailMsgService")
+    EmailMsgService gmailEmailService(){
+        return new GmailEmailMsgServiceImpl();
     }
 }
